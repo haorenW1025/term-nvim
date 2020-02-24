0d81e77f4416b888d442d03250370e19e4d552e3
haorenW1025 <whz861025@gmail.com>
2020-02-24 08:06:32 +0000

    fix some api issue

--- lua/term.lua
+++ lua/term.lua
@@ -2,49 +2,63 @@ local api =  vim.api
 local M = {}
 
 function M.term_open()
-  if M.bufID == nil or not api.nvim_buf_is_valid(M.bufID) then
+  if M.buf_handle == nil or not api.nvim_buf_is_valid(M.buf_handle) then
     api.nvim_command('botright split new') 
     api.nvim_win_set_height(0, 20)
-    M.winID = api.nvim_call_function("winnr", {})
-    M.bufID = api.nvim_call_function("bufnr", {"%"})
-    M.jobID = api.nvim_call_function("termopen", {"/bin/zsh"})
+    M.win_handle = api.nvim_tabpage_get_win(0)
+    M.buf_handle = api.nvim_win_get_buf(0)
+    M.jobID = api.nvim_call_function("termopen", {"$SHELL"})
     api.nvim_command('wincmd p')
   else
     api.nvim_command('botright split new')
     api.nvim_command('resize 20')
-    M.winID = api.nvim_call_function("winnr", {})
-    api.nvim_set_current_buf(M.bufID)
+    M.win_handle = api.nvim_tabpage_get_win(0)
+    api.nvim_set_current_buf(M.buf_handle)
     api.nvim_command('wincmd p')
   end
 end
 
 function M.term_close()
-  -- TODO
-  api.nvim_command('wincmd b')
-  api.nvim_command('close')
+  api.nvim_win_close(M.win_handle, true)
 end
 
 function M.term_toggle()
   -- TODO
-  if M.bufID == nil or api.nvim_call_function("winbufnr", {M.winID}) == -1 then
-    M.term_open() 
+  if M.buf_handle == nil or not api.nvim_win_is_valid(M.win_handle) then
+    M.term_open()
   else
     M.term_close()
   end
 end
 
 function M.term_send(command)
-  -- TODO
-  if M.bufID == nil or api.nvim_call_function("winbufnr", {M.winID}) == -1 then
+  if M.buf_handle == nil or not api.nvim_win_is_valid(M.win_handle) then
     M.term_open()
   end
   api.nvim_call_function("chansend", {M.jobID, command.."\n"})
+  api.nvim_set_current_win(M.win_handle)
+  -- force scroll down
+  api.nvim_command("normal G")
+  api.nvim_command("wincmd p")
 end
 
 -- NOTE can't send "\<c-c>" currently through lua API
 -- Use vimscript function instead
 function M.term_kill()
   api.nvim_call_function("termnvim#term_kill", {M.jobID})
+end
+
+function M.term_focus()
+  if M.buf_handle == nil or not api.nvim_win_is_valid(M.win_handle) then
+    M.term_open()
+  end
+  api.nvim_set_current_win(M.win_handle)
+  api.nvim_win_set_height(M.win_handle, 80)
+end
+
+function M.term_unfocus()
+  api.nvim_win_set_height(20)
+  api.nvim_command("wincmd p")
 end
 
 return M
--- plugin/termnvim.vim
+++ plugin/termnvim.vim
@@ -6,6 +6,7 @@ set cpo&vim
 command! -nargs=0 TermToggle lua require'term'.term_toggle()
 command! -nargs=1 TermSend lua require'term'.term_send(<f-args>)
 command! -nargs=0 TermKill lua require'term'.term_kill()
+command! -nargs=0 TermFocus lua require'term'.term_focus()
 
 let &cpo = s:save_cpo
 unlet s:save_cpo
