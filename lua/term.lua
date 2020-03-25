36e615dbca92f374f1fb3a0fc0f621a721dd46a7
haorenW1025 <whz861025@gmail.com>
2020-03-25 13:27:16 +0000

    expand command support

--- lua/term.lua
+++ lua/term.lua
@@ -1,17 +1,22 @@
+local vim = vim
 local api =  vim.api
 local M = {}
 
+local function expand_command(command)
+  return string.gsub(command, "%%", vim.fn.expand('%'))
+end
+
 function M.term_open()
   if M.buf_handle == nil or not api.nvim_buf_is_valid(M.buf_handle) then
-    api.nvim_command('botright split new') 
-    api.nvim_win_set_height(0, 20)
+    api.nvim_command('botright split new')
+    api.nvim_win_set_height(0, api.nvim_get_var('term_default_height'))
     M.win_handle = api.nvim_tabpage_get_win(0)
     M.buf_handle = api.nvim_win_get_buf(0)
     M.jobID = api.nvim_call_function("termopen", {"$SHELL"})
     api.nvim_command('wincmd p')
   else
     api.nvim_command('botright split new')
-    api.nvim_command('resize 20')
+    api.nvim_win_set_height(0, api.nvim_get_var('term_default_height'))
     M.win_handle = api.nvim_tabpage_get_win(0)
     api.nvim_set_current_buf(M.buf_handle)
     api.nvim_command('wincmd p')
@@ -35,6 +40,7 @@ function M.term_send(command)
   if M.buf_handle == nil or not api.nvim_win_is_valid(M.win_handle) then
     M.term_open()
   end
+  command = expand_command(command)
   api.nvim_call_function("chansend", {M.jobID, command.."\n"})
   api.nvim_set_current_win(M.win_handle)
   -- force scroll down
@@ -53,11 +59,11 @@ function M.term_focus()
     M.term_open()
   end
   api.nvim_set_current_win(M.win_handle)
-  api.nvim_win_set_height(M.win_handle, 80)
+  api.nvim_win_set_height(M.win_handle, api.nvim_get_var('term_focus_height'))
 end
 
 function M.term_unfocus()
-  api.nvim_win_set_height(20)
+  api.nvim_win_set_height(api.nvim_get_var('term_default_height'))
   api.nvim_command("wincmd p")
 end
 
--- plugin/termnvim.vim
+++ plugin/termnvim.vim
@@ -3,6 +3,19 @@ if exists('g:loaded_term') | finish | endif
 let s:save_cpo = &cpo
 set cpo&vim
 
+if ! exists('g:term_default_height')
+    " disable autoclose by defualt
+    let g:term_default_height = 15
+endif
+
+if ! exists('g:term_focus_height')
+    " disable autoclose by defualt
+    let g:term_focus_height = 80
+endif
+
+if ! exists('g:term_terminal_mode_toggle')
+    let g:term_terminal_mode_toggle = '<c-t>'
+endif
 command! -nargs=0 TermToggle lua require'term'.term_toggle()
 command! -nargs=1 TermSend lua require'term'.term_send(<f-args>)
 command! -nargs=0 TermKill lua require'term'.term_kill()
